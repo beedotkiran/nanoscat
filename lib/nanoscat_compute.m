2702b0a8a4fd140dee207d9cf16e44ba0cba5800
Ravi Kiran <beedotkiran@gmail.com>
2016-06-25 14:37:04 +0200

    removed path variable and simplified code
    path{ic} is a cell array that was used to store filtering for current
    order m and place it at the cell U{m+1}.
    This was removed since it added no intuition or interpretability.
    
    NOTE : subsampling the filtering with psi is still missing

--- lib/nanoscat_compute.m
+++ lib/nanoscat_compute.m
@@ -1,33 +1,28 @@
 function [S, U] = nanoscat_compute (sig, psi, phi, M)
 U = {};
 S = {};
-
+%initialise U(1,1) (U0 in paper) with signal before starting the loop
 U{1}{1}= sig;
 log2N = log2 (length (psi{1}{1})); % maximal length
 J = numel(psi);
 
 for m = 1:M+1
+    %this is the index of lambda (or j) for the wavelet filters 
     hindex = 1;
-    
     for s = 1:numel(U{m})
         sigf = fft (U{m}{s});
+        %res here always calculates to 1
         res = (log2N - (log2 (length(sigf)))) + 1;
-
+        %Convolution with \psi_\lambda wavelet filters
         if m<=M
-            vindex = 1;
-            paths = {};
             for j = s : numel(psi{res})
                 ds = 2^(j-s);
                 c = abs(ifft(sigf .* psi{res}{j}));
-                paths{vindex} = c;
-                vindex = vindex + 1;
-            end
-            
-            for ic=1:numel(paths)
-                U{m+1}{hindex}=paths{ic};
-                hindex=hindex+1;
+                U{m+1}{hindex} = c;
+                hindex = hindex + 1;
             end
         end
+        %Convolution with \phi_J (low pass) at various resolutions res
         ds = (J - res)^2;
         c = abs (ifft(sigf .* phi{res}));
         if ds > 1
